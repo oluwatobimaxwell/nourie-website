d939e9f2e29e3ac397b7cd3d4c731ef39c7f3c75
flexy777 <samuelomoyele777@gmail.com>
2025-10-15 13:19:22 +0100

    updated base endpoint

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://staging.eatnourie.com/api/", // replace with your actual backend base URL
+  baseURL: "https://production.eatnourie.com/api/", // replace with your actual backend base URL
   headers: {
     "Content-Type": "application/json",
   },
